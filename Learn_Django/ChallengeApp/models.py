a18de7efcea5c96b3f9b0689b6cfbd6182243aa8
Arjun Shome <arjunshome111@gmail.com>
2021-05-25 02:59:40 +0530

    Django Models Basic Added

--- Learn_Django/ChallengeApp/models.py
+++ Learn_Django/ChallengeApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Individual(models.Model):
+    FirstName = models.CharField(max_length=20)
+    SecondName = models.CharField(max_length=20)
+    Mobile = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return self.FirstName
+
+class Gadget(models.Model):
+    GadgetType = models.CharField(max_length = 100, unique=True)
+
+    def __str__(self):
+        return self.GadgetType
+
+class Individual_Gadget_Detail(models.Model):
+    Name = models.ForeignKey(Individual, on_delete=models.CASCADE)
+    Type = models.ForeignKey(Gadget, on_delete=models.CASCADE)
+    Brand = models.CharField(max_length = 20)
+    Buy_Date = models.DateField()
+    BoughtFrom = models.CharField(max_length=50)
+    Price = models.FloatField()
+
+    def __str__(self):
+        return self.Brand
